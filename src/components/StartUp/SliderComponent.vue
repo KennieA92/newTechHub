eeaec7d07887633ed15cd3d5232ea87272799a2e
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-05-24 16:04:19 +0200

    Edited headers in Slider Component

--- src/components/StartUp/SliderComponent.vue
+++ src/components/StartUp/SliderComponent.vue
@@ -11,21 +11,21 @@
                 <div class="carousel-item active" data-bs-interval="10000">
                     <img :src="require('@/assets/StartUp/Image1.png')" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-block">
-                        <h5>TechHub Reboot Event</h5>
+                        <h1>TechHub Reboot Event</h1>
                         <p>Tech&Tequila Event hosted at Esbjerg Streetfood by Kasper, Ronni & Trine. </p>
                     </div>
                 </div>
                 <div class="carousel-item" data-bs-interval="10000">
                     <img :src="require('@/assets/StartUp/Image2.png')" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-block">
-                        <h5>Networking Event</h5>
+                        <h1>Networking Event</h1>
                         <p>People networking at Esbjerg Streetfood, during the Tech & Tequila event. </p>
                     </div>
                 </div>
                 <div class="carousel-item" data-bs-interval="10000">
                     <img :src="require('@/assets/StartUp/Image3.png')" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-block">
-                        <h5>Engaging presentation</h5>
+                        <h1>Engaging Presentation</h1>
                         <p>People listening intently to the presentation done by Ronni. </p>
                     </div>
                 </div>
@@ -72,6 +72,15 @@ export default {
         height: 30vh;
 
         .carousel-item {
+            h1 {
+                font-size: 1.3rem;
+
+            }
+
+            p {
+                font-size: 1rem;
+            }
+
             img {
                 object-fit: scale-down;
                 object-position: unset;
