3084059c172c08162c1881541f47cac80bed2266
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-05-25 20:30:18 +0200

    Added a draft for Gallery

---
+++ src/components/Gallery/GalleryComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <GalleryModalWindow v-if="showModal" @close="toggleModal" />
+    <div class="col-md-4 col-12 p-2">
+        <div @click="toggleModal" class="">
+            <img :src="require('@/assets/StartUp/Image3.png')" class="card-img-top" alt="..." />
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+import GalleryModalWindow from "./GalleryModalWindow.vue";
+export default {
+    components: { GalleryModalWindow },
+    setup() {
+        const showModal = ref(false);
+        const toggleModal = () => {
+            showModal.value = !showModal.value;
+        };
+        return {
+            showModal,
+            toggleModal,
+        };
+    },
+};
+</script>
+<style lang="scss" scoped>
+img {
+    object-fit: cover;
+    max-height: 30vh;
+}
+</style>
---
+++ src/components/Gallery/GalleryModalWindow.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="block container" @click="$emit('close')">
+            <img :src="require('@/assets/StartUp/Image3.png')" alt="Card image cap" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() { },
+};
+</script>
+
+<style lang="scss" scoped>
+.block {
+    position: fixed;
+    z-index: 9999;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    img {
+        object-fit: contain;
+        max-width: 100%;
+        height: 80vh;
+    }
+}
+</style>
