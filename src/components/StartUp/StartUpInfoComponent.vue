ae27e8859684144d75b037104590f8a10bd5f8ce
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-05-30 10:16:24 +0300

    fixed padding StartUp

--- src/components/StartUp/StartUpInfoComponent.vue
+++ src/components/StartUp/StartUpInfoComponent.vue
@@ -1,47 +1,55 @@
 <template>
-    <section id="startup-info-section">
-        <div class="main-container d-flex flex-column col-12 justify-content-center align-items-center pt-5">
-            <h1 class="col-10 col-sm-12">Are you a <span>Tech Startup</span> looking for <span>Funding?</span></h1>
-            <p class="col-6 col-sm-4 pt-5">Then this is your chance to meet Tech-Focused investors looking for the most
-                pomising
-                <span>Tech
-                    Startups</span> in the region.
-            </p>
-            <a href="#startup-signup-section" class="button "> CONTACT US </a>
-        </div>
-    </section>
+  <section id="startup-info-section">
+    <div
+      class="
+        main-container
+        d-flex
+        flex-column
+        col-12
+        justify-content-center
+        align-items-center
+      "
+    >
+      <h1 class="col-10 col-sm-12">
+        Are you a <span>Tech Startup</span> looking for <span>Funding?</span>
+      </h1>
+      <p class="col-6 col-sm-4 pt-5">
+        Then this is your chance to meet Tech-Focused investors looking for the
+        most pomising <span>Tech Startups</span> in the region.
+      </p>
+      <a href="#startup-signup-section" class="button"> CONTACT US </a>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
 #startup-info-section {
-    padding: 5vh 0;
+  padding: 5vh 0;
 
-    p {
-        font-size: 1.5rem;
-    }
+  p {
+    font-size: 1.5rem;
+  }
 
-    .button {
-        width: 145px;
-        min-height: 45px;
-        background-color: $tertiary-color;
-        color: $quaternary-color;
-        border-style: none;
-        border-radius: 25px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: 20px;
-        text-decoration: none;
-    }
+  .button {
+    width: 145px;
+    min-height: 45px;
+    background-color: $tertiary-color;
+    color: $quaternary-color;
+    border-style: none;
+    border-radius: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    text-decoration: none;
+  }
 }
 
 span {
-    font-weight: bold;
+  font-weight: bold;
 }
 </style>
