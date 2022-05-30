fc9b03095f41e1dd276f8e4a7fa9d0cd92cf33a9
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-05-30 14:36:33 +0200

    Updated Footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -36,14 +36,14 @@
     </div>
     <div class="container">
       <div class="row">
-        <div class="col-md-8 col-sm-6 col-xs-12">
+        <div class="col-12">
           <p class="copyright-text">
             Copyright &copy; 2022 All Rights Reserved by
             <a href="#">TechHubSyd</a>.
           </p>
         </div>
 
-        <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="col-12 pt-3 d-flex justify-content-center">
           <ul class="social-icons">
             <li>
               <a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
@@ -70,49 +70,66 @@ export default {};
 
 <style lang="scss" scoped>
 .site-footer {
-  background-color: #333;
+  background-color: rgba($primary-color, 1);
   padding: 45px 0 20px;
   font-size: 15px;
   line-height: 24px;
-  color: #737373;
+  color: $quaternary-color;
+
+  a {
+    color: $quaternary-color;
+    font-size: 1.3rem;
+    font-weight: bold;
+
+  }
+
   .site-footer hr {
     border-top-color: #bbb;
     opacity: 0.5;
+
     .site-footer hr.small {
       margin: 20px 0;
     }
   }
 }
+
 .site-footer h6 {
   color: $quaternary-color;
   font-size: 16px;
   text-transform: uppercase;
   margin-top: 5px;
 }
+
 .site-footer a:hover {
   color: $primary-color;
   text-decoration: none;
 }
+
 .footer-links {
   padding-left: 0;
   list-style: none;
 }
 
 .footer-links a {
-  color: #737373;
+  color: $quaternary-color;
+
 }
+
 .footer-links a:active,
 .footer-links a:focus,
 .footer-links a:hover {
   color: $primary-color;
   text-decoration: none;
 }
+
 .footer-links.inline li {
   display: inline-block;
 }
+
 .site-footer .social-icons {
   text-align: right;
 }
+
 .site-footer .social-icons a {
   width: 40px;
   height: 40px;
@@ -120,44 +137,52 @@ export default {};
   margin-left: 6px;
   margin-right: 0;
   border-radius: 100%;
-  background-color: #333;
+  background-color: $secondary-color;
 }
+
 .copyright-text {
   margin: 0;
 }
+
 @media (max-width: 991px) {
   .site-footer [class^="col-"] {
     margin-bottom: 30px;
   }
 }
+
 @media (max-width: 767px) {
   .site-footer {
     padding-bottom: 0;
   }
+
   .site-footer .copyright-text,
   .site-footer .social-icons {
     text-align: center;
   }
 }
+
 .social-icons {
   padding-left: 0;
   margin-bottom: 0;
   list-style: none;
 }
+
 .social-icons li {
   display: inline-block;
   margin-bottom: 4px;
 }
+
 .social-icons li.title {
   margin-right: 15px;
   text-transform: uppercase;
-  color: #96a2b2;
+  color: $quaternary-color;
   font-weight: 700;
   font-size: 13px;
 }
+
 .social-icons a {
-  background-color: #eceeef;
-  color: #818a91;
+  background-color: $quaternary-color;
+  color: $quaternary-color;
   font-size: 16px;
   display: inline-block;
   line-height: 44px;
@@ -170,30 +195,37 @@ export default {};
   -o-transition: all 0.2s linear;
   transition: all 0.2s linear;
 }
+
 .social-icons a:active,
 .social-icons a:focus,
 .social-icons a:hover {
   color: #fff;
-  background-color: #29aafe;
+  background-color: $primary-color;
 }
+
 .social-icons.size-sm a {
   line-height: 34px;
   height: 34px;
   width: 34px;
   font-size: 14px;
 }
+
 .social-icons a.facebook:hover {
   background-color: #3b5998;
 }
+
 .social-icons a.twitter:hover {
   background-color: #00aced;
 }
+
 .social-icons a.linkedin:hover {
   background-color: #007bb6;
 }
+
 .social-icons a.dribbble:hover {
   background-color: #ea4c89;
 }
+
 @media (max-width: 767px) {
   .social-icons li.title {
     display: block;
